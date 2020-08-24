b91ab70cd8266cbeb7caf80548a79681956a7929
wesleycardososilva <silvawesley@protonmail.com>
2020-08-24 20:20:18 -0300

    Consulta simples com query methods

--- src/main/java/com/wesley/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wesley/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.wesley.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.wesley.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post,String>{
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/wesley/workshopmongo/services/PostService.java
+++ src/main/java/com/wesley/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.wesley.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,4 +23,7 @@ public class PostService {
 		return user;		
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
