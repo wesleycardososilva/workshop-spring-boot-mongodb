1154fa52b3c17f346a802946e4e713da3ebb936e
wesleycardososilva <silvawesley@protonmail.com>
2020-08-24 16:06:09 -0300

    Obtendo um post por id

--- src/main/java/com/wesley/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesley/workshopmongo/resources/UserResource.java
@@ -63,10 +63,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
 	@RequestMapping(value = "/{id}/posts", method=RequestMethod.GET)
 	public ResponseEntity<List<Post>>   findPosts(@PathVariable String id){
 		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
 	}
+	
 }
---
+++ src/main/java/com/wesley/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.wesley.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesley.workshopmongo.domain.Post;
+import com.wesley.workshopmongo.repository.PostRepository;
+import com.wesley.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontradao");
+		}
+		return user;		
+	}
+	
+}
