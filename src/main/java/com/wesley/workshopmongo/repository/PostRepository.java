836a108903eaed43325ddddedd72dc36990df7b8
wesleycardososilva <silvawesley@protonmail.com>
2020-08-25 17:35:41 -0300

    Consulta simples com @Query

--- src/main/java/com/wesley/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wesley/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,15 @@ package com.wesley.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.wesley.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post,String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
